fcd83b32eef0b8e637c3b99d57689dbc0c663d4c
Ben Brown <git@b8r.org>
2012-09-28 14:19:36 -0400

    Importing a rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Longlivesnarf::Application.config.secret_token = '4a93cf06f7c89dfb96b9f3bd9c9b52b75df070c8706863ed209d1d4ec43958818b79593ea0ba724c44396663488c69f66c215a3a26b2de5db3b62b246f67b845'
